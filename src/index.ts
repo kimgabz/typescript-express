b0ddc8214e03ad1770bf3f8bc50818ed50788695
kim <khemgabz@gmail.com>
2020-05-27 13:20:53 +0800

    Basic route with express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('hi there');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+const PORT = 3000;
+
+const resHTML = `
+    <div>
+        <h1>Hello Typescript world!</h1>
+    </div>
+`;
+app.get('/', (req: Request, res: Response) => {
+    res.send(resHTML);
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
