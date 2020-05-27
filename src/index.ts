b302812bab940a34f91ae6e0710133cd3b57db06
kim <khemgabz@gmail.com>
2020-05-27 14:02:07 +0800

    Parsing FORM bodies

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
 import { router } from './modules/login.route';
 
 const app = express();
 
 const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.use(router);
 
--- src/modules/login.route.ts
+++ src/modules/login.route.ts
@@ -3,13 +3,25 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 const resHTML = `
-    <div>
-        <h1>Hello from route - Typescript world!</h1>
-    </div>
+    <form method="POST">
+        <div>
+            <label>Email</label>
+            <input name="email"/>
+        </div>
+        <div>
+            <label>Password</label>
+            <input name="password" type="password"/>
+        </div>
+        <button>Submit</button>
+    </form>
 `;
-router.get('/', (req: Request, res: Response) => {
+router.get('/login', (req: Request, res: Response) => {
     res.send(resHTML);
 });
 
+router.post('/login', (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    res.send(email + password);
+});
 
 export { router };
